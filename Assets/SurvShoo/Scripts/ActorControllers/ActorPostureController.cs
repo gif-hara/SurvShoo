dba9c8d8ec7a5e22b5ac739f05938fb6619d614b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-05-29 23:33:31 +0900

    chore: Refactor ActorPostureController's Setup method and implement Move method

--- Assets/SurvShoo/Scripts/ActorControllers/ActorPostureController.cs
+++ Assets/SurvShoo/Scripts/ActorControllers/ActorPostureController.cs
@@ -1,3 +1,7 @@
+using R3;
+using R3.Triggers;
+using UnityEngine;
+
 namespace SurvShoo
 {
     /// <summary>
@@ -5,8 +9,27 @@ namespace SurvShoo
     /// </summary>
     public sealed class ActorPostureController
     {
+        private Vector2 velocity;
+
         public void Setup(Actor actor)
         {
+            actor.UpdateAsObservable()
+                .Subscribe(_ =>
+                {
+                    if (velocity == Vector2.zero)
+                    {
+                        return;
+                    }
+
+                    actor.transform.position += new Vector3(velocity.x, velocity.y, 0.0f);
+                    velocity = Vector2.zero;
+                })
+                .RegisterTo(actor.destroyCancellationToken);
+        }
+
+        public void Move(Vector2 velocity)
+        {
+            this.velocity += velocity;
         }
     }
 }
