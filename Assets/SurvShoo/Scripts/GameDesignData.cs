4cb568d0a73df8aa90702165b27b4e0bd78c6030
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-05-30 17:31:08 +0900

    調整

--- Assets/SurvShoo/Scripts/GameDesignData.cs
+++ Assets/SurvShoo/Scripts/GameDesignData.cs
@@ -18,15 +18,12 @@ namespace SurvShoo
         {
             [SerializeField]
             private MinMaxValue moveSpeed;
-            public MinMaxValue MoveSpeed => moveSpeed;
 
             [SerializeField]
             private float slowModeMoveSpeedRate;
-            public float SlowModeMoveSpeedRate => slowModeMoveSpeedRate;
             
             [SerializeField]
             private int moveSpeedLevelMax;
-            public int MoveSpeedLevelMax => moveSpeedLevelMax;
             
             public float GetMoveSpeedRate(int level, bool isSlowMode)
             {
