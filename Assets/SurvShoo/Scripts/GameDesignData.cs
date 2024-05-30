91bcaad930adb622329d8bdf8a3deed3dd9ec11a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-05-30 17:49:43 +0900

    弾データ定義

--- Assets/SurvShoo/Scripts/GameDesignData.cs
+++ Assets/SurvShoo/Scripts/GameDesignData.cs
@@ -13,6 +13,10 @@ namespace SurvShoo
         private _PlayerData playerData;
         public _PlayerData PlayerData => playerData;
         
+        [SerializeField]
+        private _BulletData.DictionaryList bulletData;
+        public _BulletData.DictionaryList BulletData => bulletData;
+        
         [Serializable]
         public class _PlayerData
         {
@@ -34,6 +38,46 @@ namespace SurvShoo
                 var rate = (float)level / moveSpeedLevelMax;
                 var slowModeRate = isSlowMode ? slowModeMoveSpeedRate : 1.0f;
                 return moveSpeed.Evaluate(rate) * slowModeRate;
+            }
+        }
+
+        [Serializable]
+        public class _BulletData
+        {
+            [SerializeField]
+            private string id;
+            public string Id => id;
+            
+            [SerializeField]
+            private FloatData.DictionaryList database;
+            public FloatData.DictionaryList Database => database;
+
+            [Serializable]
+            public class DictionaryList : DictionaryList<string, _BulletData>
+            {
+                public DictionaryList(Func<_BulletData, string> idSelector) : base(idSelector)
+                {
+                }
+            }
+        }
+
+        [Serializable]
+        public class FloatData
+        {
+            [SerializeField]
+            private string id;
+            public string Id => id;
+            
+            [SerializeField]
+            private float data;
+            public float Data => data;
+            
+            [Serializable]
+            public class DictionaryList : DictionaryList<string, FloatData>
+            {
+                public DictionaryList(Func<FloatData, string> idSelector) : base(x => x.id)
+                {
+                }
             }
         }
     }
