1c8723082a3723b35e216938af881df1229607c7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-05-30 15:13:07 +0900

    とりあえず移動するように

--- Assets/SurvShoo/Scripts/GamePlayerController.cs
+++ Assets/SurvShoo/Scripts/GamePlayerController.cs
@@ -1,4 +1,7 @@
+using R3;
 using R3.Triggers;
+using unity1week202403;
+using UnityEngine;
 
 namespace SurvShoo
 {
@@ -9,6 +12,14 @@ namespace SurvShoo
     {
         public void Setup(Actor actor)
         {
+            var inputController = TinyServiceLocator.Resolve<InputController>();
+            actor.UpdateAsObservable()
+                .Subscribe(_ =>
+                {
+                    var velocity = inputController.InputActions.Game.Move.ReadValue<Vector2>();
+                    actor.transform.position += new Vector3(velocity.x, velocity.y, 0) * Time.deltaTime * 100.0f;
+                })
+                .RegisterTo(actor.destroyCancellationToken);
         }
     }
 }
