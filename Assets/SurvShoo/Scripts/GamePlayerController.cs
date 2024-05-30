57eac83e033b04c7f6af3afd476f332afbcb366f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-05-30 14:23:07 +0900

    GamePlayerController

---
+++ Assets/SurvShoo/Scripts/GamePlayerController.cs
@@ -0,0 +1,14 @@
+using R3.Triggers;
+
+namespace SurvShoo
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class GamePlayerController
+    {
+        public void Setup(Actor actor)
+        {
+        }
+    }
+}
