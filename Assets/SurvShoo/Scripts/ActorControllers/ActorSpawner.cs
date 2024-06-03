5f069826ef9abaf3a66fc802c070cf550808b1f7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-06-03 22:52:20 +0900

    refactor: Implement object pooling for Actor class

---
+++ Assets/SurvShoo/Scripts/ActorControllers/ActorSpawner.cs
@@ -0,0 +1,29 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace SurvShoo
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [CreateAssetMenu(menuName = "SurvShoo/ActorSpawner")]
+    public sealed class ActorSpawner : ScriptableObject
+    {
+        [SerializeField]
+        private Actor actorPrefab;
+
+        [SerializeField]
+        private ScriptableSequences onEnterSequences;
+
+        public void Spawn(Vector3 position, Quaternion rotation)
+        {
+            var actor = actorPrefab.RentToPool();
+            actor.transform.position = position;
+            actor.transform.rotation = rotation;
+            var container = new Container();
+            var sequencer = new Sequencer(container, onEnterSequences.Sequences);
+            sequencer.PlayAsync(actor.poolCancellationToken).Forget();
+        }
+    }
+}
