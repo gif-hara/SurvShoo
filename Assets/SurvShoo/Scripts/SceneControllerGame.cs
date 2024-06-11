11410614f86addad6a856277b286f8f812c7bbcb
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-06-11 22:28:51 +0900

    feat: Add debug enemy spawner in SceneControllerGame.cs

--- Assets/SurvShoo/Scripts/SceneControllerGame.cs
+++ Assets/SurvShoo/Scripts/SceneControllerGame.cs
@@ -13,6 +13,12 @@ namespace SurvShoo
         [SerializeField]
         private GameInstanceData gameInstanceData;
 
+        [SerializeField]
+        private ActorSpawner debugEnemySpawner;
+
+        [SerializeField]
+        private Transform debugEnemySpawnerPoint;
+
         async void Start()
         {
             await BootSystem.IsReady;
@@ -21,6 +27,11 @@ namespace SurvShoo
             TinyServiceLocator.Register(gameDesignData);
             TinyServiceLocator.Register(new ActorPool());
             gameDesignData.PlayerData.ActorSpawner.Spawn(Vector3.zero, Quaternion.identity);
+
+            if (debugEnemySpawner != null && debugEnemySpawnerPoint != null)
+            {
+                debugEnemySpawner.Spawn(debugEnemySpawnerPoint.position, debugEnemySpawnerPoint.rotation);
+            }
         }
     }
 }
