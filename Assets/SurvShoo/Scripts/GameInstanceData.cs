ea2d3f8ac276653a5bee041d243c0bde1853829d
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-06-13 17:05:22 +0900

    reactiveproperty

--- Assets/SurvShoo/Scripts/GameInstanceData.cs
+++ Assets/SurvShoo/Scripts/GameInstanceData.cs
@@ -1,4 +1,6 @@
 using System;
+using Cysharp.Threading.Tasks;
+using R3;
 using UnityEngine;
 
 namespace SurvShoo
@@ -11,14 +13,68 @@ namespace SurvShoo
     {
         [SerializeField]
         private int playerMoveSpeedLevel;
-        public int PlayerMoveSpeedLevel { get => playerMoveSpeedLevel; set => playerMoveSpeedLevel = value; }
+        
+        private ReactiveProperty<int> playerMoveSpeedLevelReactiveProperty;
+
+        public int PlayerMoveSpeedLevel
+        {
+            get => playerMoveSpeedLevel;
+            set
+            {
+                playerMoveSpeedLevel = value;
+                playerMoveSpeedLevelReactiveProperty ??= new ReactiveProperty<int>();
+                playerMoveSpeedLevelReactiveProperty.Value = value;
+            }
+        }
+
+        public ReadOnlyReactiveProperty<int> PlayerMoveSpeedLevelAsObservable()
+        {
+            playerMoveSpeedLevelReactiveProperty ??= new ReactiveProperty<int>(playerMoveSpeedLevel);
+            return playerMoveSpeedLevelReactiveProperty;
+        }
 
         [SerializeField]
         private int playerFireCooldownLevel;
-        public int PlayerFireCooldownLevel { get => playerFireCooldownLevel; set => playerFireCooldownLevel = value; }
+        
+        private ReactiveProperty<int> playerFireCooldownLevelReactiveProperty;
+
+        public int PlayerFireCooldownLevel
+        {
+            get => playerFireCooldownLevel;
+            set
+            {
+                playerFireCooldownLevel = value;
+                playerFireCooldownLevelReactiveProperty ??= new ReactiveProperty<int>();
+                playerFireCooldownLevelReactiveProperty.Value = value;
+            }
+        }
+
+        public ReadOnlyReactiveProperty<int> PlayerFireCooldownLevelAsObservable()
+        {
+            playerFireCooldownLevelReactiveProperty ??= new ReactiveProperty<int>(playerFireCooldownLevel);
+            return playerFireCooldownLevelReactiveProperty;
+        }
 
         [SerializeField]
         private int playerBulletFirePointLevel;
-        public int PlayerBulletFirePointLevel { get => playerBulletFirePointLevel; set => playerBulletFirePointLevel = value; }
+        
+        private ReactiveProperty<int> playerBulletFirePointLevelReactiveProperty;
+
+        public int PlayerBulletFirePointLevel
+        {
+            get => playerBulletFirePointLevel;
+            set
+            {
+                playerBulletFirePointLevel = value;
+                playerBulletFirePointLevelReactiveProperty ??= new ReactiveProperty<int>();
+                playerBulletFirePointLevelReactiveProperty.Value = value;
+            }
+        }
+
+        public ReadOnlyReactiveProperty<int> PlayerBulletFirePointLevelAsObservable()
+        {
+            playerBulletFirePointLevelReactiveProperty ??= new ReactiveProperty<int>(playerBulletFirePointLevel);
+            return playerBulletFirePointLevelReactiveProperty;
+        }
     }
 }
