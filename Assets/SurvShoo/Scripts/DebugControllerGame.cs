821a65e2f201ddacc157367afb2446671c8df0a1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-06-13 23:28:14 +0900

    feat: Improve player control debug functionality

--- Assets/SurvShoo/Scripts/DebugControllerGame.cs
+++ Assets/SurvShoo/Scripts/DebugControllerGame.cs
@@ -16,32 +16,32 @@ namespace SurvShoo
             Observable.EveryUpdate(scope)
                 .Subscribe(_ =>
                 {
-                    if(Keyboard.current.numpad1Key.wasPressedThisFrame)
+                    if (Keyboard.current.digit1Key.wasPressedThisFrame)
                     {
                         gameInstanceData.AddPlayerMoveSpeedLevel(1);
                         Debug.Log($"PlayerMoveSpeedLevel: {gameInstanceData.PlayerMoveSpeedLevel.Data}");
                     }
-                    if(Keyboard.current.numpad2Key.wasPressedThisFrame)
+                    if (Keyboard.current.digit2Key.wasPressedThisFrame)
                     {
                         gameInstanceData.AddPlayerMoveSpeedLevel(-1);
                         Debug.Log($"PlayerMoveSpeedLevel: {gameInstanceData.PlayerMoveSpeedLevel.Data}");
                     }
-                    if(Keyboard.current.numpad3Key.wasPressedThisFrame)
+                    if (Keyboard.current.digit3Key.wasPressedThisFrame)
                     {
                         gameInstanceData.AddPlayerFireCooldownLevel(1);
                         Debug.Log($"PlayerFireCooldownLevel: {gameInstanceData.PlayerFireCooldownLevel.Data}");
                     }
-                    if(Keyboard.current.numpad4Key.wasPressedThisFrame)
+                    if (Keyboard.current.digit4Key.wasPressedThisFrame)
                     {
                         gameInstanceData.AddPlayerFireCooldownLevel(-1);
                         Debug.Log($"PlayerFireCooldownLevel: {gameInstanceData.PlayerFireCooldownLevel.Data}");
                     }
-                    if(Keyboard.current.numpad5Key.wasPressedThisFrame)
+                    if (Keyboard.current.digit5Key.wasPressedThisFrame)
                     {
                         gameInstanceData.AddPlayerBulletFirePointLevel(1);
                         Debug.Log($"PlayerBulletFirePointLevel: {gameInstanceData.PlayerBulletFirePointLevel.Data}");
                     }
-                    if(Keyboard.current.numpad6Key.wasPressedThisFrame)
+                    if (Keyboard.current.digit6Key.wasPressedThisFrame)
                     {
                         gameInstanceData.AddPlayerBulletFirePointLevel(-1);
                         Debug.Log($"PlayerBulletFirePointLevel: {gameInstanceData.PlayerBulletFirePointLevel.Data}");
